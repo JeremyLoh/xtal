0cce1f8b68cad5515515337820ce3db62ee28ef6
JeremyLoh <jeremyloh03@gmail.com>
2024-11-19 22:44:14 +0800

    feat: change map tile layer to OpenStreetMap

--- frontend/src/features/map/components/Map/Map.tsx
+++ frontend/src/features/map/components/Map/Map.tsx
@@ -6,17 +6,12 @@ import { useEffect } from "react"
 function Map() {
   useEffect(() => {
     const map = L.map("map").setView([1.35, 103.81], 4)
-    const stadiaStamenToner = L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.{ext}",
-      {
-        minZoom: 4,
-        maxZoom: 15,
-        //@ts-ignore
-        ext: "png",
-        attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }
-    ).addTo(map)
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      minZoom: 4,
+      maxZoom: 15,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map)
     return () => {
       map.remove()
     }
