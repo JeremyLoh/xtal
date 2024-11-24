1c1d30f5c41fbaf4e91620849400adaf555fbe50
JeremyLoh <jeremyloh03@gmail.com>
2024-11-24 18:04:30 +0800

    fix: increase webServer timeout

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173",
+    // baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -76,7 +76,7 @@ export default defineConfig({
   webServer: {
     command: "npm run preview",
     url: "http://localhost:5173",
-    timeout: 60000,
+    timeout: 120000,
     reuseExistingServer: !process.env.CI,
   },
 })
