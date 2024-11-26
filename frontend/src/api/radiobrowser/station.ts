37a4c491572aba155bb6695a47261bae2a138dcc
JeremyLoh <jeremyloh03@gmail.com>
2024-11-26 16:48:40 +0800

    feat: add min bitrate and random station offset between 0 and 10000

--- frontend/src/api/radiobrowser/station.ts
+++ frontend/src/api/radiobrowser/station.ts
@@ -4,14 +4,20 @@ import { Station } from "./types"
 import { convertMissingInformation } from "./converter/stationConverter"
 
 async function getRandomStation(): Promise<Station> {
+  const minBitrate = 64
+  const offset = getRandomInt(10000)
   const searchParams = new URLSearchParams(
-    "?order=random&limit=1&hidebroken=true&is_https=true"
+    `?order=random&limit=1&hidebroken=true&is_https=true&bitrateMin=${minBitrate}&offset=${offset}`
   )
   const server = await getRandomServer()
   const url = `${server}/json/stations/search`
   const json: Station[] = await ky.get(url, { retry: 0, searchParams }).json()
   const station = convertMissingInformation(json[0])
   return station
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
 }
 
 export { getRandomStation }
