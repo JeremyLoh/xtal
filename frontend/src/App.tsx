8f08449cc655cfe9f323049771f151563f409306
JeremyLoh <jeremyloh03@gmail.com>
2024-11-26 22:00:07 +0800

    feat: disable get random station button during loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,8 +9,10 @@ import { Station } from "./api/radiobrowser/types"
 function App() {
   const abortControllerRef = useRef<AbortController | null>(null)
   const [currentStation, setCurrentStation] = useState<Station | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   async function displayRandomStation() {
+    setIsLoading(true)
     abortControllerRef.current?.abort()
     abortControllerRef.current = new AbortController()
     const station = await getRandomStation(abortControllerRef.current)
@@ -21,11 +23,15 @@ function App() {
     } else {
       // TODO display error message for failure to get station
     }
+    setIsLoading(false)
   }
   return (
     <>
       <Header />
-      <RadioSelect handleRandomSelect={displayRandomStation} />
+      <RadioSelect
+        handleRandomSelect={displayRandomStation}
+        isLoading={isLoading}
+      />
       <Map station={currentStation} />
     </>
   )
