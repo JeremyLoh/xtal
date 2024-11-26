06a672004bada9fd6258127633b61ff80f6c32ba
JeremyLoh <jeremyloh03@gmail.com>
2024-11-26 16:56:28 +0800

    feat: retrieve 3 random stations and randomly select one

--- frontend/src/api/radiobrowser/station.ts
+++ frontend/src/api/radiobrowser/station.ts
@@ -6,13 +6,14 @@ import { convertMissingInformation } from "./converter/stationConverter"
 async function getRandomStation(): Promise<Station> {
   const minBitrate = 64
   const offset = getRandomInt(10000)
+  const limit = 3
   const searchParams = new URLSearchParams(
-    `?order=random&limit=1&hidebroken=true&is_https=true&bitrateMin=${minBitrate}&offset=${offset}`
+    `?order=random&limit=${limit}&hidebroken=true&is_https=true&bitrateMin=${minBitrate}&offset=${offset}`
   )
   const server = await getRandomServer()
   const url = `${server}/json/stations/search`
   const json: Station[] = await ky.get(url, { retry: 0, searchParams }).json()
-  const station = convertMissingInformation(json[0])
+  const station = convertMissingInformation(json[getRandomInt(limit - 1)])
   return station
 }
 
