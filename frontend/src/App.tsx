2fc6baa5f3beabfd34516f5be525e7e443d64489
JeremyLoh <jeremyloh03@gmail.com>
2024-11-26 21:23:49 +0800

    feat: add abort controller for getting random station

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { getRandomStation } from "./api/radiobrowser/station"
 import "./App.css"
 import Map from "./features/map/components/Map/Map"
@@ -7,14 +7,19 @@ import Header from "./components/Header/Header"
 import { Station } from "./api/radiobrowser/types"
 
 function App() {
+  const abortControllerRef = useRef<AbortController | null>(null)
   const [currentStation, setCurrentStation] = useState<Station | null>(null)
 
   async function displayRandomStation() {
-    const station = await getRandomStation()
+    abortControllerRef.current?.abort()
+    abortControllerRef.current = new AbortController()
+    const station = await getRandomStation(abortControllerRef.current)
     if (station) {
       // TODO remove debug log
       console.log(JSON.stringify(station, null, 2))
       setCurrentStation(station)
+    } else {
+      // TODO display error message for failure to get station
     }
   }
   return (
--- frontend/src/api/radiobrowser/station.ts
+++ frontend/src/api/radiobrowser/station.ts
@@ -3,7 +3,9 @@ import { getRandomServer } from "./servers"
 import { Station } from "./types"
 import { convertMissingInformation } from "./converter/stationConverter"
 
-async function getRandomStation(): Promise<Station> {
+async function getRandomStation(
+  abortController: AbortController
+): Promise<Station | null> {
   const minBitrate = 64
   const offset = getRandomInt(10000)
   const limit = 3
@@ -12,11 +14,21 @@ async function getRandomStation(): Promise<Station> {
   )
   const server = await getRandomServer()
   const url = `${server}/json/stations/search`
-  const json: Station[] = await ky.get(url, { retry: 0, searchParams }).json()
-  // API might return less entries compared to limit (reduce by 1 for array zero based index)
-  const responseCount = Math.max(json.length - 1, 0)
-  const station = convertMissingInformation(json[getRandomInt(responseCount)])
-  return station
+  try {
+    const json: Station[] = await ky
+      .get(url, { retry: 0, signal: abortController.signal, searchParams })
+      .json()
+    // API might return less entries compared to limit (reduce by 1 for array zero based index)
+    const responseCount = Math.max(json.length - 1, 0)
+    const station = convertMissingInformation(json[getRandomInt(responseCount)])
+    return station
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error.name === "AbortError") {
+      console.log("Aborted getRandomStation request")
+    }
+    return null
+  }
 }
 
 function getRandomInt(max: number) {
