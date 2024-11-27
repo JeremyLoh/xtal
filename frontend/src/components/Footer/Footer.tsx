5bc6d9962a111a6d567ca73413fa129581d85469
JeremyLoh <jeremyloh03@gmail.com>
2024-11-27 16:42:06 +0800

    feat: update light mode color scheme

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,15 +1,17 @@
+import { FaGithubSquare } from "react-icons/fa"
 import "./Footer.css"
 
 function Footer() {
   return (
     <footer>
+      <span>&copy; Jeremy_Loh</span>
       <a
         id="footer-github-link"
         href="https://github.com/JeremyLoh/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        @Jeremy_Loh
+        <FaGithubSquare size={28} />
       </a>
     </footer>
   )
