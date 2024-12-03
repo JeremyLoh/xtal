0ea2f4bf57c1125f287a46fd0781605d7fdca804
JeremyLoh <jeremyloh03@gmail.com>
2024-12-03 22:59:18 +0800

    feat: update header border bottom width style

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,13 +3,15 @@ import "./Header.css"
 
 function Header() {
   return (
-    <header>
-      <p className="header-app-name">Xtal</p>
-      <div className="header-info">
-        <p>Listen to the World</p>
-        <ThemeToggle />
-      </div>
-    </header>
+    <div id="header-container">
+      <header>
+        <p className="header-app-name">Xtal</p>
+        <div className="header-info">
+          <p>Listen to the World</p>
+          <ThemeToggle />
+        </div>
+      </header>
+    </div>
   )
 }
 
