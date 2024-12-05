14057e6caf7a4470daac320e48b88ab33dd6accd
JeremyLoh <jeremyloh03@gmail.com>
2024-12-05 13:18:48 +0800

    fix: increase test wait time for map navigation to one second

--- frontend/tests/homepage.filter.station.spec.ts
+++ frontend/tests/homepage.filter.station.spec.ts
@@ -82,7 +82,7 @@ test.describe("radio station search type", () => {
       await page.goto(HOMEPAGE)
       await getCountrySearchButton(page).click()
       await clickRandomRadioStationButton(page)
-      await page.waitForTimeout(500) // test fails without waiting for the navigation
+      await page.waitForTimeout(1000) // test fails without waiting for the navigation
       const expectedStationMapPane =
         (await page
           .locator(".leaflet-proxy.leaflet-zoom-animated")
