9322b9294479d2de888d9c31916e26e54cab39d9
JeremyLoh <jeremyloh03@gmail.com>
2025-02-07 23:52:22 +0800

    feat: prevent duplicate episode card description html elements from strict mode

--- frontend/src/components/PodcastEpisodeCard/PodcastEpisodeCard.tsx
+++ frontend/src/components/PodcastEpisodeCard/PodcastEpisodeCard.tsx
@@ -43,10 +43,18 @@ export default function PodcastEpisodeCard({
   )
 }
 
+function isHTML(text: string) {
+  const document = new DOMParser().parseFromString(text, "text/html")
+  return Array.from(document.body.childNodes).some(
+    (node) => node.nodeType === Node.ELEMENT_NODE
+  )
+}
+
 function convertHtmlStringToElement(htmlString: string) {
+  // should only be used for string that contain HTML!
+  // https://stackoverflow.com/questions/29643368/cannot-append-dom-element-to-div-node-uncaught-hierarchyrequesterror-failed-to
   const domParser = new DOMParser()
   const document = domParser.parseFromString(htmlString, "text/html")
-  // https://stackoverflow.com/questions/29643368/cannot-append-dom-element-to-div-node-uncaught-hierarchyrequesterror-failed-to
   return document.documentElement
 }
 
@@ -74,8 +82,27 @@ PodcastEpisodeCard.Title = function PodcastEpisodeCardTitle() {
 PodcastEpisodeCard.Description = function PodcastEpisodeCardDescription() {
   const { episode, descriptionDivRef } = usePodcastEpisodeCardContext()
   useEffect(() => {
+    if (
+      descriptionDivRef.current == null ||
+      descriptionDivRef.current.hasChildNodes()
+    ) {
+      // hasChildNodes() prevents React strict mode from appending duplicate episode description
+      return
+    }
+    if (!isHTML(episode.description)) {
+      const paragraph = document.createElement("p")
+      paragraph.innerText = episode.description
+      descriptionDivRef.current.appendChild(paragraph)
+      return
+    }
+    const fragment = document.createDocumentFragment()
     const element = convertHtmlStringToElement(episode.description)
-    descriptionDivRef.current?.appendChild(element)
+    for (const child of element.children) {
+      if (child.tagName.toLowerCase() === "body") {
+        fragment.append(...child.childNodes)
+      }
+    }
+    descriptionDivRef.current.appendChild(fragment)
   }, [descriptionDivRef, episode.description])
 
   return (
