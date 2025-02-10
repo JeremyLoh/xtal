757315e4427a36b664d532857323fa020e3dc057
JeremyLoh <jeremyloh03@gmail.com>
2025-02-10 17:51:43 +0800

    feat: add podcast episode context

---
+++ frontend/src/context/PodcastEpisodeProvider/PodcastEpisodeProvider.tsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react"
+import { PodcastEpisode } from "../../api/podcast/model/podcast"
+
+type PodcastEpisodeInfo = {
+  episode: PodcastEpisode | null
+  setEpisode: React.Dispatch<React.SetStateAction<PodcastEpisode | null>>
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const PodcastEpisodeContext = createContext<PodcastEpisodeInfo | null>(
+  null
+)
+
+export default function PodcastEpisodeProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [episode, setEpisode] = useState<PodcastEpisode | null>(null)
+  const value: PodcastEpisodeInfo = {
+    episode,
+    setEpisode,
+  }
+  return (
+    <PodcastEpisodeContext.Provider value={value}>
+      {children}
+    </PodcastEpisodeContext.Provider>
+  )
+}
