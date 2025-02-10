2ba02aa813775e304ce754dbbdbf837ace4ec7e4
JeremyLoh <jeremyloh03@gmail.com>
2025-02-10 23:24:02 +0800

    feat: add error message for episode playback error

--- frontend/src/components/AudioPlayer/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,8 +1,9 @@
 import "./AudioPlayer.css"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useState } from "react"
 import {
   MediaControlBar,
   MediaController,
+  MediaErrorDialog,
   MediaMuteButton,
   MediaPlaybackRateButton,
   MediaPlayButton,
@@ -18,47 +19,60 @@ type AudioPlayerProps = PropsWithChildren & {
 }
 
 function AudioPlayer(props: AudioPlayerProps) {
+  const [error, setError] = useState<boolean>(false)
   return (
     <div className="audio-player">
       {props.children}
       <MediaController audio>
-        <audio slot="media" src={props.source} autoPlay></audio>
+        <audio
+          slot="media"
+          src={props.source}
+          autoPlay
+          onError={() => setError(props.source !== "")}
+          onCanPlay={() => setError(false)}
+        ></audio>
 
-        <MediaControlBar style={{ padding: "0 0.5rem" }}>
-          <div className="mobile">
-            <MediaPlayButton data-testid="audio-player-mobile-play-button" />
-            <MediaSeekBackwardButton
-              seekOffset={15}
-              data-testid="audio-player-mobile-seek-backward-button"
-            />
-            <MediaPlaybackRateButton data-testid="audio-player-mobile-playback-rate-button" />
-            <MediaTimeDisplay
-              showDuration
-              data-testid="audio-player-mobile-time-display-button"
-            />
-            <MediaMuteButton data-testid="audio-player-mobile-mute-button" />
-          </div>
+        {error ? (
+          <>
+            <MediaErrorDialog />
+          </>
+        ) : (
+          <MediaControlBar style={{ padding: "0 0.5rem" }}>
+            <div className="mobile">
+              <MediaPlayButton data-testid="audio-player-mobile-play-button" />
+              <MediaSeekBackwardButton
+                seekOffset={15}
+                data-testid="audio-player-mobile-seek-backward-button"
+              />
+              <MediaPlaybackRateButton data-testid="audio-player-mobile-playback-rate-button" />
+              <MediaTimeDisplay
+                showDuration
+                data-testid="audio-player-mobile-time-display-button"
+              />
+              <MediaMuteButton data-testid="audio-player-mobile-mute-button" />
+            </div>
 
-          <div className="desktop">
-            <MediaPlayButton data-testid="audio-player-desktop-play-button" />
-            <MediaSeekBackwardButton
-              seekOffset={15}
-              data-testid="audio-player-desktop-seek-backward-button"
-            />
-            <MediaSeekForwardButton
-              seekOffset={15}
-              data-testid="audio-player-desktop-seek-forward-button"
-            />
-            <MediaTimeRange data-testid="audio-player-desktop-time-range-button" />
-            <MediaTimeDisplay
-              showDuration
-              data-testid="audio-player-desktop-time-display-button"
-            />
-            <MediaPlaybackRateButton data-testid="audio-player-desktop-playback-rate-button" />
-            <MediaMuteButton data-testid="audio-player-desktop-mute-button" />
-            <MediaVolumeRange data-testid="audio-player-desktop-volume-range-button" />
-          </div>
-        </MediaControlBar>
+            <div className="desktop">
+              <MediaPlayButton data-testid="audio-player-desktop-play-button" />
+              <MediaSeekBackwardButton
+                seekOffset={15}
+                data-testid="audio-player-desktop-seek-backward-button"
+              />
+              <MediaSeekForwardButton
+                seekOffset={15}
+                data-testid="audio-player-desktop-seek-forward-button"
+              />
+              <MediaTimeRange data-testid="audio-player-desktop-time-range-button" />
+              <MediaTimeDisplay
+                showDuration
+                data-testid="audio-player-desktop-time-display-button"
+              />
+              <MediaPlaybackRateButton data-testid="audio-player-desktop-playback-rate-button" />
+              <MediaMuteButton data-testid="audio-player-desktop-mute-button" />
+              <MediaVolumeRange data-testid="audio-player-desktop-volume-range-button" />
+            </div>
+          </MediaControlBar>
+        )}
       </MediaController>
     </div>
   )
