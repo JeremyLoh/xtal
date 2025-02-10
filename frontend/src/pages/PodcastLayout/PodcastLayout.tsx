dc7667770f4c62d7cdfab3a6094ca24e922ff2fe
JeremyLoh <jeremyloh03@gmail.com>
2025-02-10 23:47:24 +0800

    feat: add sticky podcast audio player and glass background

--- frontend/src/pages/PodcastLayout/PodcastLayout.tsx
+++ frontend/src/pages/PodcastLayout/PodcastLayout.tsx
@@ -1,10 +1,13 @@
+import "./PodcastLayout.css"
 import { Outlet } from "react-router"
 import PodcastPlayer from "../../features/podcast/player/PodcastPlayer"
 
 export default function PodcastLayout() {
   return (
     <>
-      <PodcastPlayer />
+      <div className="sticky-top">
+        <PodcastPlayer />
+      </div>
       <Outlet />
     </>
   )
