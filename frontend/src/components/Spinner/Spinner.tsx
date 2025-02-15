0bb44672f8775aa538992a3e9a9102d88d7aa11f
JeremyLoh <jeremyloh03@gmail.com>
2025-02-16 01:12:16 +0800

    feat: add spinner component

---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,31 @@
+import "./Spinner.css"
+import { AnimatePresence, motion } from "motion/react"
+import { PiSpinner } from "react-icons/pi"
+
+type SpinnerProps = {
+  isLoading: boolean
+}
+
+const Spinner = ({ isLoading }: SpinnerProps) => {
+  return (
+    <AnimatePresence>
+      {isLoading && (
+        <motion.div className="spinner-container" exit={{ opacity: 0 }}>
+          <div className="spinner-content">
+            <motion.div
+              className="spinner"
+              initial={{ rotate: 0 }}
+              animate={{ rotate: 360 }}
+              transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
+            >
+              <PiSpinner size={120} />
+            </motion.div>
+            <span className="spinner-text">LOADING</span>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Spinner
