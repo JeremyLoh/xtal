97c05a16402e243fbfdde3e1cf5a485d953a1fc5
JeremyLoh <jeremyloh03@gmail.com>
2025-02-18 12:10:39 +0800

    fix: eslint error for error boundary component

--- frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -18,17 +18,16 @@ export default class ErrorBoundary extends React.Component<
     this.state = { hasError: false }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(error: Error) {
     // https://react.dev/reference/react/Component#static-getderivedstatefromerror
     // Update state so the next render will show the fallback UI
+    console.error(error.message)
     return { hasError: true }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  componentDidCatch(error: Error, info: React.ErrorInfo) {
-    // perform side effects when error is caught by error boundary, e.g. logging and analytics
-  }
+  // componentDidCatch(error: Error, info: React.ErrorInfo) {
+  //   // perform side effects when error is caught by error boundary, e.g. logging and analytics
+  // }
 
   render() {
     if (this.state.hasError) {
