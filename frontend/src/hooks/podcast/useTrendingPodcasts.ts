a162b01597cf7989354e1d727a212f504eb96788
JeremyLoh <jeremyloh03@gmail.com>
2025-02-24 19:52:14 +0800

    refactor: extract trending podcast retrieval to useTrendingPodcasts hook

---
+++ frontend/src/hooks/podcast/useTrendingPodcasts.ts
@@ -0,0 +1,88 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+import { toast } from "sonner"
+import dayjs from "dayjs"
+import { TrendingPodcast } from "../../api/podcast/model/podcast.ts"
+import { getTrendingPodcasts } from "../../api/podcast/trendingPodcast.ts"
+
+type UseTrendingPodcastsProps = {
+  limit: number
+  category?: string
+}
+
+function convertToDate(daysBefore: number): Date {
+  return dayjs().startOf("day").subtract(daysBefore, "days").toDate()
+}
+
+function useTrendingPodcasts({ limit, category }: UseTrendingPodcastsProps) {
+  const DEFAULT_SINCE_DAYS = 3
+  const abortController = useRef<AbortController | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [trendingPodcasts, setTrendingPodcasts] = useState<
+    TrendingPodcast[] | null
+  >(null)
+
+  const getPodcasts = useCallback(
+    async (since: Date) => {
+      setLoading(true)
+      abortController.current?.abort()
+      abortController.current = new AbortController()
+      try {
+        const params = {
+          limit: limit,
+          since: since,
+          category: category,
+        }
+        const podcasts = await getTrendingPodcasts(
+          abortController.current,
+          params
+        )
+        if (podcasts && podcasts.data) {
+          setTrendingPodcasts(podcasts.data)
+        } else {
+          setTrendingPodcasts(null)
+          setLoading(false) // prevent infinite load on no data
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        toast.error(error.message)
+        setLoading(false) // prevent infinite load on error
+      }
+    },
+    [category, limit]
+  )
+
+  const handlePodcastRefresh = useCallback(
+    async (
+      filters: {
+        since: number
+        category?: string
+      } | null
+    ) => {
+      if (filters == null) {
+        await getPodcasts(convertToDate(DEFAULT_SINCE_DAYS))
+      } else {
+        const { since } = filters
+        // setSinceDaysBefore(since) // TODO handle the since change in the parent component instead of here
+        await getPodcasts(convertToDate(since))
+      }
+    },
+    [getPodcasts]
+  )
+
+  useEffect(() => {
+    // update loading state to false after set state has been run on the trending podcasts
+    // prevents display of "no podcasts available" element due to trendingPodcasts = null, and loading = false
+    if (trendingPodcasts) {
+      setLoading(false)
+    }
+  }, [trendingPodcasts])
+
+  return {
+    DEFAULT_SINCE_DAYS,
+    loading,
+    trendingPodcasts,
+    onRefresh: handlePodcastRefresh,
+  }
+}
+
+export default useTrendingPodcasts
