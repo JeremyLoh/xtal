6433e29467e9b0fe64b0cac979585f9e7000c322
JeremyLoh <jeremyloh03@gmail.com>
2025-02-24 20:57:25 +0800

    refactor: add usePodcastCategory hook to PodcastHomePage

--- frontend/src/pages/podcast/PodcastHomePage/PodcastHomePage.tsx
+++ frontend/src/pages/podcast/PodcastHomePage/PodcastHomePage.tsx
@@ -1,12 +1,10 @@
 import "./PodcastHomePage.css"
-import { useCallback, useEffect, useRef, useState } from "react"
-import { toast } from "sonner"
+import { useCallback, useEffect, useState } from "react"
 import TrendingPodcastSection from "../../../features/podcast/trending/components/TrendingPodcastSection/TrendingPodcastSection.tsx"
 import PodcastCategorySection from "../../../features/podcast/category/components/PodcastCategorySection/PodcastCategorySection.tsx"
-import { PodcastCategory } from "../../../api/podcast/model/podcast.ts"
-import { getAllPodcastCategories } from "../../../api/podcast/podcastCategory.ts"
 import Spinner from "../../../components/Spinner/Spinner.tsx"
 import useTrendingPodcasts from "../../../hooks/podcast/useTrendingPodcasts.ts"
+import usePodcastCategory from "../../../hooks/podcast/usePodcastCategory.ts"
 
 export default function PodcastHomePage() {
   const limit = 10
@@ -14,36 +12,17 @@ export default function PodcastHomePage() {
     DEFAULT_SINCE_DAYS,
     loading: loadingPodcasts,
     trendingPodcasts,
-    onRefresh,
+    onRefresh: handleTrendingPodcastRefresh,
   } = useTrendingPodcasts({
     limit: limit,
   })
-  const abortControllerCategory = useRef<AbortController | null>(null)
   const [sinceDaysBefore, setSinceDaysBefore] =
     useState<number>(DEFAULT_SINCE_DAYS)
-  const [loadingCategories, setLoadingCategories] = useState<boolean>(true)
-  const [categories, setCategories] = useState<PodcastCategory[] | null>(null)
-
-  const getPodcastCategories = useCallback(async () => {
-    setLoadingCategories(true)
-    abortControllerCategory.current?.abort()
-    abortControllerCategory.current = new AbortController()
-    try {
-      const categories = await getAllPodcastCategories(
-        abortControllerCategory.current
-      )
-      if (categories) {
-        setCategories(categories)
-      } else {
-        setCategories(null)
-        setLoadingCategories(false) // prevent infinite load on no data
-      }
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      toast.error(error.message)
-      setLoadingCategories(false) // prevent infinite load on error
-    }
-  }, [])
+  const {
+    loading: loadingCategories,
+    categories,
+    onRefresh: handlePodcastCategoryRefresh,
+  } = usePodcastCategory()
 
   const handlePodcastRefresh = useCallback(
     async (
@@ -56,29 +35,18 @@ export default function PodcastHomePage() {
         const { since } = filters
         setSinceDaysBefore(since)
       }
-      await onRefresh(filters)
+      await handleTrendingPodcastRefresh(filters)
     },
-    [onRefresh]
+    [handleTrendingPodcastRefresh]
   )
 
   useEffect(() => {
     document.title = "xtal - podcasts"
     Promise.allSettled([
-      getPodcastCategories(),
+      handlePodcastCategoryRefresh(),
       handlePodcastRefresh({ since: sinceDaysBefore }),
     ])
-    return () => {
-      abortControllerCategory.current?.abort()
-    }
-  }, [getPodcastCategories, handlePodcastRefresh, sinceDaysBefore])
-
-  useEffect(() => {
-    // update the category loading state to false after the state has been updated
-    // prevents display of "no categories available" element due to categories = null, and loading = false
-    if (categories) {
-      setLoadingCategories(false)
-    }
-  }, [categories])
+  }, [handlePodcastCategoryRefresh, handlePodcastRefresh, sinceDaysBefore])
 
   return (
     <div id="podcast-home-page-container">
@@ -86,7 +54,7 @@ export default function PodcastHomePage() {
       <PodcastCategorySection
         loading={loadingCategories}
         categories={categories}
-        onRefresh={getPodcastCategories}
+        onRefresh={handlePodcastCategoryRefresh}
       />
       <TrendingPodcastSection
         loading={loadingPodcasts}
