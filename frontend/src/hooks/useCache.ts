945f42b7ecc9b8da56865cd844e54401560b83db
JeremyLoh <jeremyloh03@gmail.com>
2025-02-27 14:54:57 +0800

    feat: add cache clear of expiry items during cache get operation

--- frontend/src/hooks/useCache.ts
+++ frontend/src/hooks/useCache.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import useSessionStorage from "./useSessionStorage.ts"
 
 type CacheType<V> = {
-  lastFetchedEpochMs: number
+  expiryEpochMs: number
   value: V
 }
 
@@ -11,37 +11,53 @@ function useCache<V>(key: string, cacheStaleTimeInMinutes: number) {
   const { getItem, setItem, removeItem } = useSessionStorage(key)
   const setCacheItem = useCallback(
     (value: V) => {
-      setItem({ lastFetchedEpochMs: dayjs().unix(), value })
+      setItem({
+        expiryEpochMs: dayjs().add(cacheStaleTimeInMinutes, "minute").unix(),
+        value,
+      })
     },
-    [setItem]
+    [setItem, cacheStaleTimeInMinutes]
   )
 
   const getCacheItem = useCallback(() => {
+    clearExpiredCacheItems()
+
     const cachedData = getItem() as CacheType<V>
     if (cachedData == null) {
       return null
     }
     const currentTimestamp = dayjs()
-    const cachedTimestamp = dayjs.unix(cachedData.lastFetchedEpochMs)
-    const expiredTimestamp = cachedTimestamp.add(
-      cacheStaleTimeInMinutes,
-      "minute"
-    )
-    if (cachedData && currentTimestamp.isBefore(expiredTimestamp)) {
+    const cachedExpiredTimestamp = dayjs.unix(cachedData.expiryEpochMs)
+    if (cachedData && currentTimestamp.isBefore(cachedExpiredTimestamp)) {
       return cachedData
-    } else if (cachedData && currentTimestamp.isAfter(expiredTimestamp)) {
+    } else if (cachedData && currentTimestamp.isAfter(cachedExpiredTimestamp)) {
       removeItem()
       return null
     } else {
       return null
     }
-  }, [getItem, removeItem, cacheStaleTimeInMinutes])
+  }, [getItem, removeItem])
 
   const cacheFunctions = useMemo(() => {
     return { setCacheItem, getCacheItem }
   }, [setCacheItem, getCacheItem])
 
   return cacheFunctions
+}
+
+function clearExpiredCacheItems() {
+  const currentTimestamp = dayjs()
+  for (const key of Object.keys(sessionStorage)) {
+    const savedValue = sessionStorage.getItem(key)
+    if (!savedValue) {
+      return
+    }
+    const json = JSON.parse(savedValue)
+    const expiryTimestamp = dayjs.unix(json.expiryEpochMs)
+    if (json.expiryEpochMs && currentTimestamp.isAfter(expiryTimestamp)) {
+      sessionStorage.removeItem(key)
+    }
+  }
 }
 
 export default useCache
