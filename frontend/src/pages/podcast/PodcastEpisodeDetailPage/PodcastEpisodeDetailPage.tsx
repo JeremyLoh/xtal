57bcbf832d89afccf5c119f513377ffeba6a9255
JeremyLoh <jeremyloh03@gmail.com>
2025-03-03 23:11:41 +0800

    feat: add back button for podcast episode detail page

--- frontend/src/pages/podcast/PodcastEpisodeDetailPage/PodcastEpisodeDetailPage.tsx
+++ frontend/src/pages/podcast/PodcastEpisodeDetailPage/PodcastEpisodeDetailPage.tsx
@@ -1,13 +1,15 @@
 import "./PodcastEpisodeDetailPage.css"
 import { useCallback, useContext, useEffect } from "react"
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
+import { IoArrowBackSharp } from "react-icons/io5"
 import { PodcastEpisodeContext } from "../../../context/PodcastEpisodeProvider/PodcastEpisodeProvider.tsx"
 import LoadingDisplay from "../../../components/LoadingDisplay/LoadingDisplay.tsx"
 import PodcastEpisodeCard from "../../../components/PodcastEpisodeCard/PodcastEpisodeCard.tsx"
 import usePodcastEpisode from "../../../hooks/podcast/usePodcastEpisode.ts"
 
 export default function PodcastEpisodeDetailPage() {
-  const { podcastEpisodeId } = useParams()
+  const { podcastId, podcastTitle, podcastEpisodeId } = useParams()
+  const navigate = useNavigate()
   const podcastEpisodeContext = useContext(PodcastEpisodeContext)
   const { loading, episode, fetchPodcastEpisode } = usePodcastEpisode()
 
@@ -23,23 +25,39 @@ export default function PodcastEpisodeDetailPage() {
     }
   }, [podcastEpisodeContext, episode])
 
+  const handleBackButtonNavigation = useCallback(() => {
+    if (podcastId && podcastTitle) {
+      const podcastDetailPageUrl = `/podcasts/${podcastTitle}/${podcastId}`
+      navigate(podcastDetailPageUrl)
+    }
+  }, [navigate, podcastTitle, podcastId])
+
   return (
     <LoadingDisplay loading={loading}>
       <div className="podcast-episode-detail-container">
         {episode && (
-          <PodcastEpisodeCard episode={episode}>
-            <PodcastEpisodeCard.Artwork
-              size={200}
-              title={episode.title + " podcast image"}
-            />
-            <PodcastEpisodeCard.Title />
-            <PodcastEpisodeCard.PublishDate />
-            <PodcastEpisodeCard.Duration />
-            <PodcastEpisodeCard.ExplicitIndicator />
-            <PodcastEpisodeCard.EpisodeWebsiteLink />
-            <PodcastEpisodeCard.PlayButton onPlayClick={handlePlayClick} />
-            <PodcastEpisodeCard.Description className="podcast-episode-detail-description" />
-          </PodcastEpisodeCard>
+          <>
+            <button
+              className="podcast-episode-detail-back-button"
+              onClick={handleBackButtonNavigation}
+            >
+              <IoArrowBackSharp size={16} />
+              Back
+            </button>
+            <PodcastEpisodeCard episode={episode}>
+              <PodcastEpisodeCard.Artwork
+                size={200}
+                title={episode.title + " podcast image"}
+              />
+              <PodcastEpisodeCard.Title />
+              <PodcastEpisodeCard.PublishDate />
+              <PodcastEpisodeCard.Duration />
+              <PodcastEpisodeCard.ExplicitIndicator />
+              <PodcastEpisodeCard.EpisodeWebsiteLink />
+              <PodcastEpisodeCard.PlayButton onPlayClick={handlePlayClick} />
+              <PodcastEpisodeCard.Description className="podcast-episode-detail-description" />
+            </PodcastEpisodeCard>
+          </>
         )}
       </div>
     </LoadingDisplay>
--- frontend/tests/podcast/detail/podcast.episode.detail.page.spec.ts
+++ frontend/tests/podcast/detail/podcast.episode.detail.page.spec.ts
@@ -1,6 +1,9 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
 import { HOMEPAGE } from "../../constants/homepageConstants.ts"
-import { podcastId_259760_episodeId_34000697601 } from "../../mocks/podcast.episode.ts"
+import {
+  podcastId_259760_episodeId_34000697601,
+  podcastId_259760_FirstTenEpisodes,
+} from "../../mocks/podcast.episode.ts"
 import { assertPodcastEpisodeOnPodcastEpisodeDetailPage } from "../../constants/podcast/detail/podcastDetailConstants.ts"
 
 test.describe("Podcast Episode Detail Page for viewing single podcast episode /podcasts/PODCAST-TITLE/PODCAST-ID/PODCAST-EPISODE-ID", () => {
@@ -22,5 +25,43 @@ test.describe("Podcast Episode Detail Page for viewing single podcast episode /p
       page,
       podcastId_259760_episodeId_34000697601
     )
+  })
+
+  test("should navigate back to the podcast detail page when back button is clicked", async ({
+    page,
+  }) => {
+    const podcastTitle = encodeURIComponent("Infinite Loops")
+    const podcastId = "259760"
+    const podcastEpisodeId = "34000697601"
+    await page.route(
+      `*/**/api/podcast/episode?id=${podcastEpisodeId}`,
+      async (route) => {
+        const json = podcastId_259760_episodeId_34000697601
+        await route.fulfill({ json })
+      }
+    )
+    const limit = "10"
+    await page.route(
+      `*/**/api/podcast/episodes?id=${podcastId}&limit=${limit}`,
+      async (route) => {
+        // mock podcast detail page data for back button navigation
+        const json = podcastId_259760_FirstTenEpisodes
+        await route.fulfill({ json })
+      }
+    )
+
+    await page.goto(
+      HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}/${podcastEpisodeId}`
+    )
+    await expect(
+      page.locator(".podcast-episode-detail-back-button")
+    ).toBeVisible()
+    expect(page.url()).toMatch(
+      HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}/${podcastEpisodeId}`
+    )
+    await page.locator(".podcast-episode-detail-back-button").click()
+
+    const expectedUrl = HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}`
+    expect(page.url()).toMatch(expectedUrl)
   })
 })
