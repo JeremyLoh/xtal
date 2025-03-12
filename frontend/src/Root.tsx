8a153a49713296721d128bdb7aef7dbabcbd546a
JeremyLoh <jeremyloh03@gmail.com>
2025-03-13 00:35:07 +0800

    fix: build error from vercel speed insights import

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router"
 import { Toaster } from "sonner"
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Header from "./components/Header/Header.tsx"
 import Footer from "./components/Footer/Footer.tsx"
 
