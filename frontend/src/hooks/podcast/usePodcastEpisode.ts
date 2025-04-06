26fdc485717fb10d027c267bc696da722b2d0132
JeremyLoh <jeremyloh03@gmail.com>
2025-04-06 23:09:45 +0800

    fix: prevent podcast episode detail page fetch after successful request

--- frontend/src/hooks/podcast/usePodcastEpisode.ts
+++ frontend/src/hooks/podcast/usePodcastEpisode.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { useCallback, useMemo, useRef, useState } from "react"
 import { PodcastEpisode } from "../../api/podcast/model/podcast"
 import { toast } from "sonner"
 import { getPodcastEpisode } from "../../api/podcast/podcastEpisode"
@@ -11,35 +11,27 @@ function usePodcastEpisode() {
 
   const fetchPodcastEpisode = useCallback(async (episodeId: string) => {
     setLoading(true)
-    abortController.current?.abort()
+    setError(null)
     abortController.current = new AbortController()
     try {
       const params = { id: episodeId }
       const response = await getPodcastEpisode(abortController.current, params)
       if (response && response.data) {
         setEpisode(response.data)
-        setLoading(false)
       } else {
-        setLoading(false) // prevent infinite load on no data
         setError("Podcast episode data is not available")
       }
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       toast.error(error.message)
-      setLoading(false) // prevent infinite loading on error
       setError(error.message)
+    } finally {
+      setLoading(false)
     }
     return () => {
       abortController.current?.abort()
     }
   }, [])
-
-  useEffect(() => {
-    if (episode) {
-      // prevent race condition between setLoading and set podcast episode, display of "no episode found" placeholder before podcast data set state
-      setLoading(false)
-    }
-  }, [episode])
 
   const output = useMemo(() => {
     return {
