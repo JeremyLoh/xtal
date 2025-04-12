46e43bc8401c36edb3585997cb6bc1215c6f7fd2
JeremyLoh <jeremyloh03@gmail.com>
2025-04-12 16:37:50 +0800

    refactor: extract profile podcast episode history to own component

---
+++ frontend/src/features/profile/history/PodcastEpisodeHistory/PodcastEpisodeHistory.tsx
@@ -0,0 +1,75 @@
+import "./PodcastEpisodeHistory.css"
+import { memo } from "react"
+import { MdDelete } from "react-icons/md"
+import { PlayHistoryPodcastEpisode } from "../../../../hooks/podcast/usePlayHistory.ts"
+import Button from "../../../../components/ui/button/Button.tsx"
+import PodcastEpisodeCard from "../../../../components/PodcastEpisodeCard/index.tsx"
+
+type PodcastEpisodeHistoryProps = {
+  IMAGE_LAZY_LOAD_START_INDEX: number
+  episodes: PlayHistoryPodcastEpisode[] | null
+  onDelete: (episodeId: number) => Promise<void>
+}
+
+function PodcastEpisodeHistory({
+  IMAGE_LAZY_LOAD_START_INDEX,
+  episodes,
+  onDelete,
+}: PodcastEpisodeHistoryProps) {
+  if (episodes == null || episodes.length === 0) {
+    return <p>Not available. Start listening to some podcasts!</p>
+  }
+
+  return (
+    <div className="profile-history-podcast-episodes-container">
+      {episodes.map((data, index) => {
+        const episode = data.podcastEpisode
+        const podcastTitle = episode.feedTitle
+        const podcastId = episode.feedId
+        return (
+          <div
+            className="profile-history-podcast-listen-history-item"
+            key={`${episode.id}-item`}
+          >
+            <span className="profile-history-podcast-listen-history-count">
+              {index + 1}
+            </span>
+            <PodcastEpisodeCard key={episode.id} episode={episode}>
+              <PodcastEpisodeCard.Artwork
+                size={144}
+                title={`${episode.title} podcast image`}
+                lazyLoad={index >= IMAGE_LAZY_LOAD_START_INDEX}
+              />
+              <PodcastEpisodeCard.Title
+                url={`/podcasts/${podcastTitle}/${podcastId}/${episode.id}`}
+              />
+              <PodcastEpisodeCard.PublishDate />
+              <PodcastEpisodeCard.Duration />
+              <PodcastEpisodeCard.ExplicitIndicator />
+              <PodcastEpisodeCard.EpisodeWebsiteLink />
+              <PodcastEpisodeCard.EpisodeNumber />
+              <PodcastEpisodeCard.SeasonNumber />
+            </PodcastEpisodeCard>
+            <div className="profile-history-podcast-episode-actions">
+              <span>
+                Last Played on {data.lastPlayedTimestamp.toDateString()}
+              </span>
+              <Button
+                onClick={() => onDelete(episode.id)}
+                data-testid={`profile-history-delete-button-podcast-episode-${episode.id}`}
+                className="profile-history-delete-podcast-episode-button"
+                variant="danger"
+                title="Delete podcast episode from listen history"
+              >
+                <MdDelete size={24} />
+                Delete
+              </Button>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default memo(PodcastEpisodeHistory)
--- frontend/src/pages/ProfileHistoryPage/ProfileHistoryPage.tsx
+++ frontend/src/pages/ProfileHistoryPage/ProfileHistoryPage.tsx
@@ -1,13 +1,11 @@
 import "./ProfileHistoryPage.css"
 import { useEffect, useState } from "react"
-import { MdDelete } from "react-icons/md"
 import { toast } from "sonner"
 import LoadingDisplay from "../../components/LoadingDisplay/LoadingDisplay.tsx"
 import usePlayHistory, {
   PlayHistoryPodcastEpisode,
 } from "../../hooks/podcast/usePlayHistory.ts"
-import PodcastEpisodeCard from "../../components/PodcastEpisodeCard/index.tsx"
-import Button from "../../components/ui/button/Button.tsx"
+import PodcastEpisodeHistory from "../../features/profile/history/PodcastEpisodeHistory/PodcastEpisodeHistory.tsx"
 
 const IMAGE_LAZY_LOAD_START_INDEX = 2
 
@@ -38,70 +36,25 @@ function ProfileHistoryPage() {
     })
   }, [session, getPlayedPodcastEpisodes])
 
+  const handlePodcastEpisodeDelete = async (episodeId: number) => {
+    if (episodes == null) {
+      return
+    }
+    await deletePlayedPodcastEpisode(`${episodeId}`)
+    setEpisodes(episodes.filter((e) => e.podcastEpisode.id !== episodeId))
+    toast.success("Deleted podcast episode from listen history")
+  }
+
   return (
     <div className="profile-history-page-container">
       <h2 className="profile-history-page-title">Profile History</h2>
       <h3 className="profile-history-page-title">Podcast Listen History</h3>
       <LoadingDisplay loading={loading}>
-        {episodes && episodes.length > 0 ? (
-          <div className="profile-history-podcast-episodes-container">
-            {episodes.map((data, index) => {
-              const episode = data.podcastEpisode
-              const podcastTitle = episode.feedTitle
-              const podcastId = episode.feedId
-              const handlePodcastEpisodeDelete = async () => {
-                await deletePlayedPodcastEpisode(`${episode.id}`)
-                setEpisodes(
-                  episodes.filter((e) => e.podcastEpisode.id !== episode.id)
-                )
-                toast.success("Deleted podcast episode from listen history")
-              }
-              return (
-                <div
-                  className="profile-history-podcast-listen-history-item"
-                  key={`${episode.id}-item`}
-                >
-                  <span className="profile-history-podcast-listen-history-count">
-                    {index + 1}
-                  </span>
-                  <PodcastEpisodeCard key={episode.id} episode={episode}>
-                    <PodcastEpisodeCard.Artwork
-                      size={144}
-                      title={`${episode.title} podcast image`}
-                      lazyLoad={index >= IMAGE_LAZY_LOAD_START_INDEX}
-                    />
-                    <PodcastEpisodeCard.Title
-                      url={`/podcasts/${podcastTitle}/${podcastId}/${episode.id}`}
-                    />
-                    <PodcastEpisodeCard.PublishDate />
-                    <PodcastEpisodeCard.Duration />
-                    <PodcastEpisodeCard.ExplicitIndicator />
-                    <PodcastEpisodeCard.EpisodeWebsiteLink />
-                    <PodcastEpisodeCard.EpisodeNumber />
-                    <PodcastEpisodeCard.SeasonNumber />
-                  </PodcastEpisodeCard>
-                  <div className="profile-history-podcast-episode-actions">
-                    <span>
-                      Last Played on {data.lastPlayedTimestamp.toDateString()}
-                    </span>
-                    <Button
-                      onClick={handlePodcastEpisodeDelete}
-                      data-testid={`profile-history-delete-button-podcast-episode-${episode.id}`}
-                      className="profile-history-delete-podcast-episode-button"
-                      variant="danger"
-                      title="Delete podcast episode from listen history"
-                    >
-                      <MdDelete size={24} />
-                      Delete
-                    </Button>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        ) : (
-          <p>Not available. Start listening to some podcasts!</p>
-        )}
+        <PodcastEpisodeHistory
+          IMAGE_LAZY_LOAD_START_INDEX={IMAGE_LAZY_LOAD_START_INDEX}
+          episodes={episodes}
+          onDelete={handlePodcastEpisodeDelete}
+        />
       </LoadingDisplay>
     </div>
   )
