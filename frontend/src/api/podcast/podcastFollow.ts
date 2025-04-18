669dbc6f5c1140950676cfc9f11a1e5628f9712d
JeremyLoh <jeremyloh03@gmail.com>
2025-04-18 20:29:29 +0800

    feat: add frontend follow podcast button on podcast detail page

---
+++ frontend/src/api/podcast/podcastFollow.ts
@@ -0,0 +1,68 @@
+import ky from "ky"
+import { Podcast } from "./model/podcast.ts"
+import { getEnv } from "../env/environmentVariables.ts"
+
+async function getPodcastFollowStatusById(
+  abortController: AbortController,
+  podcastId: string
+) {
+  const { BACKEND_ORIGIN } = getEnv()
+  const url = BACKEND_ORIGIN + "/api/podcast/follow"
+  const searchParams = new URLSearchParams(`podcastId=${podcastId}`)
+  try {
+    const response = await ky.get(url, {
+      searchParams,
+      retry: 0,
+      signal: abortController.signal,
+    })
+    const json: { isFollowing: boolean } = await response.json()
+    return json.isFollowing
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error.name === "AbortError") {
+      return null
+    }
+    if (error.response && error.response.status === 429) {
+      throw new Error(
+        `Podcast Check Follow History Rate Limit Exceeded, please try again later`
+      )
+    }
+    throw new Error(
+      "Could not check follow podcast history. Please try again later"
+    )
+  }
+}
+
+async function addPodcastFollow(
+  abortController: AbortController,
+  podcast: Podcast
+) {
+  const { BACKEND_ORIGIN } = getEnv()
+  const url = BACKEND_ORIGIN + "/api/podcast/follow"
+  const data = {
+    podcastId: podcast.id,
+    externalWebsiteUrl: podcast.url,
+    title: podcast.title,
+    author: podcast.author,
+    image: podcast.image,
+    language: podcast.language,
+    ...(podcast.episodeCount && { episodeCount: podcast.episodeCount }),
+    // publishDateUnixTimestamp is not commonly available in the Podcast type (from PodcastIndex API)
+  }
+  try {
+    await ky.post(url, { json: data, retry: 0, signal: abortController.signal })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error.name === "AbortError") {
+      return null
+    }
+    if (error.response && error.response.status === 429) {
+      throw new Error(
+        `Podcast Follow Rate Limit Exceeded, please try again later`
+      )
+    }
+    throw new Error("Could not follow podcast. Please try again later")
+  }
+}
+
+export { getPodcastFollowStatusById, addPodcastFollow }
