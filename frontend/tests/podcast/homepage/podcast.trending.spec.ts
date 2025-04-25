ad3da2e66d3f946e380922e0200a4aa6a74cac12
JeremyLoh <jeremyloh03@gmail.com>
2025-04-25 13:47:25 +0800

    fix: flaky and slow tests in headless mode

--- frontend/tests/podcast/homepage/podcast.trending.spec.ts
+++ frontend/tests/podcast/homepage/podcast.trending.spec.ts
@@ -275,6 +275,7 @@ test.describe("Podcast Homepage /podcasts", () => {
       test("should fetch new podcast entries on change to since <select> element of 'last 24 hours'", async ({
         page,
       }) => {
+        test.slow()
         const oneDayAgo = dayjs().startOf("day").subtract(1, "days").unix()
         const threeDaysAgo = dayjs().startOf("day").subtract(3, "days").unix()
         let isFirstFetch = true
