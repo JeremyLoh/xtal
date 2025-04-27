7d81418e79ecbf2cc1bab37fe43990219d6ee64c
JeremyLoh <jeremyloh03@gmail.com>
2025-04-28 00:58:52 +0800

    fix: increase playwright test timeout

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -18,9 +18,9 @@ dotenv.config({ path: path.resolve(__dirname, ".env.local") })
  */
 export default defineConfig({
   testDir: "./tests",
-  timeout: 10_000,
+  timeout: 25_000,
   expect: {
-    timeout: 6_000,
+    timeout: 7_000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
