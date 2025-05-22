2c3e19f4115436c9d7abe7314989020b7c49abf6
JeremyLoh <jeremyloh03@gmail.com>
2025-05-22 13:08:51 +0800

    feat: add dialog to podcast episode card share button

--- frontend/src/components/PodcastEpisodeCard/ShareButton.tsx
+++ frontend/src/components/PodcastEpisodeCard/ShareButton.tsx
@@ -1,8 +1,13 @@
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import { IoShareSocialSharp } from "react-icons/io5"
 import Button from "../ui/button/Button.tsx"
 import { PodcastEpisode } from "../../api/podcast/model/podcast.ts"
 import { usePodcastEpisodeCardContext } from "./PodcastEpisodeCardContext.ts"
+import Dialog, {
+  DialogContent,
+  DialogDimBackground,
+} from "../Dialog/Dialog.tsx"
+import useClickOutside from "../../hooks/useClickOutside.ts"
 
 type PodcastEpisodeCardShareButtonProps = {
   onClick: (episode: PodcastEpisode) => void
@@ -12,20 +17,54 @@ const ShareButton = function PodcastEpisodeCardShareButton({
   onClick,
 }: PodcastEpisodeCardShareButtonProps) {
   const { episode } = usePodcastEpisodeCardContext()
+  const [open, setOpen] = useState<boolean>(false)
+  const clickOutsideRef = useClickOutside<HTMLDivElement>({
+    onClickOutside: () => {
+      if (open) {
+        setOpen(false)
+      }
+    },
+  })
   const handleClick = useCallback(() => {
+    setOpen(!open)
+  }, [open])
+  const handleCopy = useCallback(() => {
     onClick(episode)
   }, [episode, onClick])
 
   return (
-    <Button
-      keyProp={`podcast-episode-share-button-${episode.id}`}
-      data-testid="podcast-episode-share-button"
-      variant="icon"
-      onClick={handleClick}
-      title="Copy Share Podcast Episode Link"
-    >
-      <IoShareSocialSharp size={20} />
-    </Button>
+    <>
+      {open && <DialogDimBackground />}
+      <div ref={clickOutsideRef}>
+        <Button
+          keyProp={`podcast-episode-share-button-${episode.id}`}
+          data-testid="podcast-episode-share-button"
+          variant="icon"
+          title="Open Share Podcast Episode Dialog"
+          onClick={handleClick}
+        >
+          <IoShareSocialSharp size={20} />
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClick}
+          title="Share this podcast episode"
+          className="podcast-episode-share-dialog"
+        >
+          <DialogContent data-testid="podcast-episode-share-dialog-content">
+            <Button
+              keyProp={`podcast-episode-copy-link-button-${episode.id}`}
+              data-testid="podcast-episode-copy-link-button"
+              variant="primary"
+              title="Copy Share Podcast Episode Link"
+              onClick={handleCopy}
+            >
+              Copy
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
   )
 }
 
--- frontend/tests/podcast/detail/podcast.episode.detail.page.share.spec.ts
+++ frontend/tests/podcast/detail/podcast.episode.detail.page.share.spec.ts
@@ -10,7 +10,88 @@ test.describe("Share Feature of Podcast Episode Detail Page for viewing single p
       .getByTestId("podcast-episode-share-button")
   }
 
-  test("should copy podcast episode detail page url when share button is clicked", async ({
+  function getPodcastEpisodeShareDialog(page: Page) {
+    return page.getByTestId("podcast-episode-share-dialog-content")
+  }
+
+  function getPodcastEpisodeCopyLinkButton(page: Page) {
+    return page.getByTestId("podcast-episode-copy-link-button")
+  }
+
+  function getPodcastEpisodeCloseDialogButton(page: Page) {
+    return page.locator(".podcast-episode-share-dialog .dialog-close-button")
+  }
+
+  test("should open share episode dialog when share button is clicked", async ({
+    page,
+  }) => {
+    const podcastTitle = encodeURIComponent("Infinite Loops")
+    const podcastId = "259760"
+    const podcastEpisodeId = "34000697601"
+    await page.route(
+      `*/**/api/podcast/episode?id=${podcastEpisodeId}`,
+      async (route) => {
+        const json = podcastId_259760_episodeId_34000697601
+        await route.fulfill({ json })
+      }
+    )
+    await page.goto(
+      HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}/${podcastEpisodeId}`
+    )
+    await expect(getPodcastEpisodeDetailShareButton(page)).toBeVisible()
+    await getPodcastEpisodeDetailShareButton(page).click()
+    await expect(getPodcastEpisodeShareDialog(page)).toBeVisible()
+  })
+
+  test("should close share episode dialog on click outside dialog", async ({
+    page,
+  }) => {
+    const podcastTitle = encodeURIComponent("Infinite Loops")
+    const podcastId = "259760"
+    const podcastEpisodeId = "34000697601"
+    await page.route(
+      `*/**/api/podcast/episode?id=${podcastEpisodeId}`,
+      async (route) => {
+        const json = podcastId_259760_episodeId_34000697601
+        await route.fulfill({ json })
+      }
+    )
+    await page.goto(
+      HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}/${podcastEpisodeId}`
+    )
+    await expect(getPodcastEpisodeDetailShareButton(page)).toBeVisible()
+    await getPodcastEpisodeDetailShareButton(page).click()
+    await expect(getPodcastEpisodeShareDialog(page)).toBeVisible()
+    // click outside modal
+    await page.locator("body").click({ position: { x: 1, y: 1 } })
+    await expect(getPodcastEpisodeShareDialog(page)).not.toBeVisible()
+  })
+
+  test("should close share episode dialog on dialog close button click", async ({
+    page,
+  }) => {
+    const podcastTitle = encodeURIComponent("Infinite Loops")
+    const podcastId = "259760"
+    const podcastEpisodeId = "34000697601"
+    await page.route(
+      `*/**/api/podcast/episode?id=${podcastEpisodeId}`,
+      async (route) => {
+        const json = podcastId_259760_episodeId_34000697601
+        await route.fulfill({ json })
+      }
+    )
+    await page.goto(
+      HOMEPAGE + `/podcasts/${podcastTitle}/${podcastId}/${podcastEpisodeId}`
+    )
+    await expect(getPodcastEpisodeDetailShareButton(page)).toBeVisible()
+    await getPodcastEpisodeDetailShareButton(page).click()
+    await expect(getPodcastEpisodeShareDialog(page)).toBeVisible()
+    await expect(getPodcastEpisodeCloseDialogButton(page)).toBeVisible()
+    await getPodcastEpisodeCloseDialogButton(page).click()
+    await expect(getPodcastEpisodeShareDialog(page)).not.toBeVisible()
+  })
+
+  test("should copy podcast episode detail page url using copy button on share podcast episode dialog", async ({
     page,
   }) => {
     const podcastTitle = encodeURIComponent("Infinite Loops")
@@ -30,6 +111,9 @@ test.describe("Share Feature of Podcast Episode Detail Page for viewing single p
     )
     await expect(getPodcastEpisodeDetailShareButton(page)).toBeVisible()
     await getPodcastEpisodeDetailShareButton(page).click()
+    await expect(getPodcastEpisodeShareDialog(page)).toBeVisible()
+    await expect(getPodcastEpisodeCopyLinkButton(page)).toBeVisible()
+    await getPodcastEpisodeCopyLinkButton(page).click()
     expect(await getClipboardContent(page)).toBe(expectedPodcastEpisodeUrl)
     await assertToastMessage(page, "Link Copied")
   })
