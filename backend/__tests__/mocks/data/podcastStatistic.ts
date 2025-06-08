070cccc222cf1b78857c6c07d202014fc66714e8
JeremyLoh <jeremyloh03@gmail.com>
2025-06-08 18:22:51 +0800

    refactor: move backend mock podcast statistic data to data directory

---
+++ backend/__tests__/mocks/data/podcastStatistic.ts
@@ -0,0 +1,14 @@
+// https://podcastindex-org.github.io/docs-api/#get-/stats/current
+export const CURRENT_PODCAST_STATISTICS = {
+  status: "true",
+  stats: {
+    feedCountTotal: 4257865,
+    episodeCountTotal: 143953477,
+    feedsWithNewEpisodes3days: 97522,
+    feedsWithNewEpisodes10days: 212322,
+    feedsWithNewEpisodes30days: 306748,
+    feedsWithNewEpisodes90days: 436933,
+    feedsWithValueBlocks: 21916,
+  },
+  description: "Found matching feed",
+}
--- backend/__tests__/mocks/podcastStatHandler.ts
+++ backend/__tests__/mocks/podcastStatHandler.ts
@@ -1,18 +1,5 @@
 import { http, HttpHandler, HttpResponse } from "msw"
-
-export const CURRENT_PODCAST_STATISTICS = {
-  status: "true",
-  stats: {
-    feedCountTotal: 4257865,
-    episodeCountTotal: 143953477,
-    feedsWithNewEpisodes3days: 97522,
-    feedsWithNewEpisodes10days: 212322,
-    feedsWithNewEpisodes30days: 306748,
-    feedsWithNewEpisodes90days: 436933,
-    feedsWithValueBlocks: 21916,
-  },
-  description: "Found matching feed",
-}
+import { CURRENT_PODCAST_STATISTICS } from "./data/podcastStatistic.js"
 
 export const podcastStatsHandlers: HttpHandler[] = [
   http.get("https://api.podcastindex.org/api/1.0/stats/current", () => {
