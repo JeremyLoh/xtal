4437094c1b217bdbd974f217f738740996fada26
JeremyLoh <jeremyloh03@gmail.com>
2025-06-20 21:19:22 +0800

    feat: update backend status endpoint rate limit

--- backend/middleware/rateLimiter.ts
+++ backend/middleware/rateLimiter.ts
@@ -131,8 +131,8 @@ const getPodcastRecentLimiter = rateLimit({
 })
 
 const getStatusLimiter = rateLimit({
-  windowMs: 30 * 1000,
-  limit: 2, // Limit each IP to "X" requests per window
+  windowMs: 2 * 1000,
+  limit: 1, // Limit each IP to "X" requests per window
   standardHeaders: "draft-7",
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: (
@@ -141,7 +141,7 @@ const getStatusLimiter = rateLimit({
     next: NextFunction,
     options: Options
   ) => {
-    res.setHeader("retry-after", 30) // in seconds
+    res.setHeader("retry-after", 2) // in seconds
     res.status(options.statusCode).send(options.message)
   },
 })
