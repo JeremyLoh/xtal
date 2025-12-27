e3a767bdbaa3e685ac8c3daf73a38e9d24302fd7
JeremyLoh <jeremyloh03@gmail.com>
2025-12-27 14:43:52 +0800

    fix: move test util function out of describe block

--- frontend/tests/homepage.app.theme.spec.ts
+++ frontend/tests/homepage.app.theme.spec.ts
@@ -9,43 +9,40 @@ test.use({
   colorScheme: "dark",
 })
 
+async function assertElementHasLightTheme(page: Page, elementSelector: string) {
+  await expect(
+    page.locator(elementSelector),
+    "should have light theme background color"
+  ).toHaveCSS("background-color", "rgb(253, 250, 252)")
+  await expect(
+    page.locator(elementSelector),
+    "should have light theme text color"
+  ).toHaveCSS("color", "rgb(24, 12, 21)")
+}
+
+async function assertElementHasDarkTheme(page: Page, elementSelector: string) {
+  await expect(
+    page.locator(elementSelector),
+    "should have dark theme background color"
+  ).toHaveCSS("background-color", "rgb(14, 15, 8)")
+  await expect(
+    page.locator(elementSelector),
+    "should have dark theme text color"
+  ).toHaveCSS("color", "rgb(246, 247, 236)")
+}
+
 test.describe("header app theme (start with dark mode)", () => {
-  async function assertElementHasLightTheme(
-    page: Page,
-    elementSelector: string
-  ) {
-    await expect(
-      page.locator(elementSelector),
-      "should have light theme background color"
-    ).toHaveCSS("background-color", "rgb(253, 250, 252)")
-    await expect(
-      page.locator(elementSelector),
-      "should have light theme text color"
-    ).toHaveCSS("color", "rgb(24, 12, 21)")
-  }
-
-  async function assertElementHasDarkTheme(
-    page: Page,
-    elementSelector: string
-  ) {
-    await expect(
-      page.locator(elementSelector),
-      "should have dark theme background color"
-    ).toHaveCSS("background-color", "rgb(14, 15, 8)")
-    await expect(
-      page.locator(elementSelector),
-      "should have dark theme text color"
-    ).toHaveCSS("color", "rgb(246, 247, 236)")
-  }
-
   test("should switch theme when app theme button is clicked", async ({
     homePage,
   }) => {
     await homePage.goto()
+
     await assertLoadingSpinnerIsMissing(homePage.getPage())
     await assertElementHasDarkTheme(homePage.getPage(), "#root")
     await expect(homePage.getAppThemeToggleButton("dark")).toBeVisible()
+
     await homePage.toggleAppTheme()
+
     await expect(homePage.getAppThemeToggleButton("light")).toBeVisible()
     await expect(homePage.getAppThemeToggleButton("dark")).not.toBeVisible()
     await assertElementHasLightTheme(homePage.getPage(), "#root")
@@ -55,9 +52,13 @@ test.describe("header app theme (start with dark mode)", () => {
     homePage,
   }) => {
     await homePage.goto()
+
     await assertLoadingSpinnerIsMissing(homePage.getPage())
+
     await homePage.openFavouriteStationsDrawer()
+
     await assertElementHasDarkTheme(homePage.getPage(), "#drawer-root")
+
     await homePage.closeDrawer()
     await homePage.toggleAppTheme()
     await homePage.openFavouriteStationsDrawer()
@@ -68,11 +69,15 @@ test.describe("header app theme (start with dark mode)", () => {
     homePage,
   }) => {
     await homePage.goto()
+
     await assertLoadingSpinnerIsMissing(homePage.getPage())
     await homePage.getSearchStationButton().click()
+
     await assertElementHasDarkTheme(homePage.getPage(), "#drawer-root")
     await homePage.closeDrawer()
+
     await homePage.toggleAppTheme()
+
     await homePage.getSearchStationButton().click()
     await assertElementHasLightTheme(homePage.getPage(), "#drawer-root")
   })
@@ -81,6 +86,7 @@ test.describe("header app theme (start with dark mode)", () => {
     homePage,
   }) => {
     await homePage.goto()
+
     await assertLoadingSpinnerIsMissing(homePage.getPage())
     await assertElementHasDarkTheme(homePage.getPage(), "#root")
     await homePage.toggleAppTheme()
@@ -96,6 +102,7 @@ test.describe("header app theme (start with dark mode)", () => {
     homePage,
   }) => {
     await homePage.goto()
+
     await assertLoadingSpinnerIsMissing(homePage.getPage())
     await assertElementHasDarkTheme(homePage.getPage(), "#root")
     await homePage.toggleAppTheme()
