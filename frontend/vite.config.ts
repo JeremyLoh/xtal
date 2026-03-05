49543c246ae3f3fcd5dede6779da370bef69ba7a
JeremyLoh <jeremyloh03@gmail.com>
2026-03-05 23:30:30 +0800

    feat: add jsdom package and @tests path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -65,6 +65,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
+        "@tests": path.resolve(__dirname, "tests"),
       },
     },
     define: {
