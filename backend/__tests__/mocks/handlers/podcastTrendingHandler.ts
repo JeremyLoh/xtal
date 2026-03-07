da3a4711e120f2d7b4b5ad2cb216dbba41d77d4a
JeremyLoh <jeremyloh03@gmail.com>
2026-03-07 18:51:43 +0800

    refactor: replace trending podcast global msw handler and place in tests

--- backend/__tests__/mocks/handlers/podcastTrendingHandler.ts
+++ backend/__tests__/mocks/handlers/podcastTrendingHandler.ts
@@ -1,28 +1,34 @@
 import { http, HttpHandler, HttpResponse } from "msw"
-import {
-  PODCAST_TRENDING_DEFAULT_TEN_ENTRIES,
-  PODCAST_TRENDING_TEN_ARTS_PODCASTS,
-} from "../data/podcast.js"
+import { PodcastIndexTrendingPodcastResponse } from "../../../api/responseType/podcastIndexPodcastTypes.js"
 
-export const podcastTrendingHandler: HttpHandler[] = [
-  http.get(
+type TrendingPodcastHandlerOptions = {
+  feeds?: PodcastIndexTrendingPodcastResponse["feeds"]
+  status?: string
+}
+
+export function createTrendingPodcastHandler(
+  options: TrendingPodcastHandlerOptions = {}
+): HttpHandler {
+  const { feeds = [], status = "true" } = options
+
+  // https://podcastindex-org.github.io/docs-api/#get-/podcasts/trending
+  return http.get(
     "https://api.podcastindex.org/api/1.0/podcasts/trending",
-    ({ request }) => {
-      // https://podcastindex-org.github.io/docs-api/#get-/podcasts/trending
-      const url = new URL(request.url)
-      const max = url.searchParams.get("max")
-      const since = url.searchParams.get("since")
-      const category = url.searchParams.get("cat")
-      if (max === "10" && since != null && category == null) {
-        return HttpResponse.json(PODCAST_TRENDING_DEFAULT_TEN_ENTRIES)
-      } else if (max === "10" && category?.toLowerCase() === "arts") {
-        return HttpResponse.json(PODCAST_TRENDING_TEN_ARTS_PODCASTS)
-      } else if (max === "15") {
-        // backend ?offset=5&limit=10 trending podcast test. PodcastIndex API doesn't have offset
-        return HttpResponse.json(PODCAST_TRENDING_DEFAULT_TEN_ENTRIES)
-      } else {
-        return HttpResponse.error()
-      }
+    () => {
+      return HttpResponse.json({
+        status,
+        feeds,
+        count: feeds.length,
+        max: feeds.length,
+        since: "1613805249",
+        description: "Found trending feeds.",
+      })
     }
-  ),
-]
+  )
+}
+
+// export const podcastTrendingHandler: HttpHandler[] = [
+// http.get("https://api.podcastindex.org/api/1.0/podcasts/trending", () =>
+//   HttpResponse.json(PODCAST_TRENDING_DEFAULT_TEN_ENTRIES)
+// ),
+// ]
