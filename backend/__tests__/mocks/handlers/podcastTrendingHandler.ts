e209358b484995099beb7723d32d637a74229992
JeremyLoh <jeremyloh03@gmail.com>
2026-03-09 00:16:58 +0800

    feat add responses to msw createTrendingPodcastHandler for simulating multiple api calls

--- backend/__tests__/mocks/handlers/podcastTrendingHandler.ts
+++ backend/__tests__/mocks/handlers/podcastTrendingHandler.ts
@@ -3,23 +3,30 @@ import { PodcastIndexTrendingPodcastResponse } from "../../../api/responseType/p
 
 type TrendingPodcastHandlerOptions = {
   feeds?: PodcastIndexTrendingPodcastResponse["feeds"]
+  responses?: PodcastIndexTrendingPodcastResponse["feeds"][]
   status?: string
 }
 
 export function createTrendingPodcastHandler(
   options: TrendingPodcastHandlerOptions = {}
 ): HttpHandler {
-  const { feeds = [], status = "true" } = options
+  const { feeds = [], responses, status = "true" } = options
+
+  let callCount = 0
 
   // https://podcastindex-org.github.io/docs-api/#get-/podcasts/trending
   return http.get(
     "https://api.podcastindex.org/api/1.0/podcasts/trending",
     () => {
+      const resultFeeds = responses?.[callCount] ?? feeds
+
+      callCount++
+
       return HttpResponse.json({
         status,
-        feeds,
-        count: feeds.length,
-        max: feeds.length,
+        feeds: resultFeeds,
+        count: resultFeeds.length,
+        max: resultFeeds.length,
         since: "1613805249",
         description: "Found trending feeds.",
       })
